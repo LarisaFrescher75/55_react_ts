62ae9f7692274bc120f1531236d535a7436ac321
LarisaFrescher75 <stas.frescher@gmail.com>
2025-03-25 22:31:25 +0100

    add homework06

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+
+import './styles.css';
+
+function Input() {
+  return <input className="Homework-input"
+  type="name"
+  placeholder="Enter your name..."
+  >Name</input>
+}
+
+export default Input
---
+++ src/homeworks/Homework06/Homework06.tsx
@@ -0,0 +1,13 @@
+
+//пример импорта по умолчанию (нужно использовать точное название и заключать его 
+//в фигурные скобки)
+import Input from "../../components/Input/Input";
+
+
+function Homework06() {
+  
+  return <Input />
+}
+
+//Экспорт по умолчанию для компонента Homework06
+export default Homework06;
